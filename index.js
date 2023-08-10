123b183f730f4fe015a5c41ee4bb87b6e7735fd1
valerine <valerine.chemutai@student.moringaschool.com>
2023-08-10 16:48:23 +0300

    objectsf

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+  name: 'Val',
+  streetAddress: '15 Broadway'
+}
+employee.name = 'Sam';
+
+  
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = Object.assign({}, employee, {[key]: value});
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = Object.assign({}, employee);
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+
+const destructivelyDeleteFromEmployeeByKey = (object, key) => {
+  delete employee[key];
+  return employee;
+}
